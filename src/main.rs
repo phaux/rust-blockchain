17329f928f9007b1806d376163afb97bc6b3444d
Nikita Stefaniak <phaux4@gmail.com>
2021-10-05 20:56:12 +0200

    Implement serialize for Blockchain

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+use serde_json::json;
+
+mod hash;
+
 #[derive(Debug)]
 struct Blockchain {
     blocks: Vec<Block>,
@@ -10,6 +14,26 @@ impl Blockchain {
 
     fn anchor(&mut self, block: Block) {
         self.blocks.push(block);
+    }
+
+    fn serialize(&self) -> String {
+        let mut prev_block_data: Option<hash::BlockData> = None;
+        let mut json_blocks = Vec::new();
+
+        for block in &self.blocks {
+            let block_data = hash::BlockData {
+                parent_hash: prev_block_data.as_ref().map(|data| data.hash()),
+                payload: &block.payload,
+            };
+            json_blocks.push(json!({
+                "prev": block_data.parent_hash.as_ref().map(|hash| base64::encode(hash)),
+                "payload": &block_data.payload,
+                "digest": base64::encode(block_data.hash()),
+            }));
+            prev_block_data = Some(block_data);
+        }
+
+        json!({ "blocks": json_blocks }).to_string()
     }
 }
 
@@ -30,5 +54,5 @@ fn main() {
     let mut bc = Blockchain::new();
     bc.anchor(Block::new("block 1"));
     bc.anchor(Block::new("block 2"));
-    dbg!(bc);
+    println!("{}", bc.serialize());
 }
