abaf8d071e4480d8425e0e995228d44f11ac7e3a
Nikita Stefaniak <phaux4@gmail.com>
2021-10-05 17:31:59 +0200

    Add Block and Blockchain

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
+#[derive(Debug)]
+struct Blockchain {
+    blocks: Vec<Block>,
+}
+
+impl Blockchain {
+    fn new() -> Self {
+        Self { blocks: Vec::new() }
+    }
+
+    fn anchor(&mut self, block: Block) {
+        self.blocks.push(block);
+    }
+}
+
+#[derive(Debug)]
+struct Block {
+    payload: String,
+}
+
+impl Block {
+    fn new(payload: &str) -> Self {
+        Self {
+            payload: payload.to_owned(),
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut bc = Blockchain::new();
+    bc.anchor(Block::new("block 1"));
+    bc.anchor(Block::new("block 2"));
+    dbg!(bc);
 }
