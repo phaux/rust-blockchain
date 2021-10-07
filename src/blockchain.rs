f4fdcf16e555603e422cd43b3e7502bbcae1756d
Nikita Stefaniak <phaux4@gmail.com>
2021-10-07 19:37:51 +0200

    Use serde derive macro instead

--- src/blockchain.rs
+++ src/blockchain.rs
@@ -1,12 +1,14 @@
-use serde::{de, ser::SerializeStruct, Deserialize, Serialize};
+use serde::{Deserialize, Serialize};
 
-use self::hash::{get_hash, Hash, HashInput};
-
-mod hash;
-
-#[derive(Debug)]
+#[derive(Debug, Clone, Serialize, Deserialize)]
+#[serde(from = "BlockchainData", into = "BlockchainData")]
 pub struct Blockchain {
     blocks: Vec<Block>,
+}
+
+#[derive(Debug, Clone)]
+pub struct Block {
+    payload: String,
 }
 
 impl Blockchain {
@@ -19,14 +21,32 @@ impl Blockchain {
     }
 }
 
-impl Serialize for Blockchain {
-    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
-    where
-        S: serde::Serializer,
-    {
-        let mut blocks = Vec::new();
-        let mut prev_block: Option<HashInput> = None;
+impl Block {
+    pub fn new(payload: &str) -> Self {
+        Self {
+            payload: payload.to_owned(),
+        }
+    }
+}
 
+#[derive(Debug, Serialize, Deserialize)]
+struct BlockchainData {
+    blocks: Vec<BlockData>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct BlockData {
+    #[serde(with = "digest::opt")]
+    prev: Option<[u8; 32]>,
+    payload: String,
+    #[serde(with = "digest")]
+    digest: [u8; 32],
+}
+
+impl Into<BlockchainData> for Blockchain {
+    fn into(self) -> BlockchainData {
+        let mut blocks = Vec::new();
+        let mut prev_block = None;
         for block in &self.blocks {
             let prev_hash = prev_block.as_ref().map(get_hash);
             let current_block = HashInput {
@@ -36,73 +56,71 @@ impl Serialize for Blockchain {
             let current_hash = get_hash(&current_block);
             blocks.push(BlockData {
                 prev: current_block.prev.clone(),
-                payload: current_block.payload,
-                digest: current_hash.clone(),
+                payload: current_block.payload.to_owned(),
+                digest: (current_hash),
             });
             prev_block = Some(current_block);
         }
-
-        let mut blockchain = serializer.serialize_struct("Blockchain", 1)?;
-        blockchain.serialize_field("blocks", &blocks)?;
-        blockchain.end()
+        BlockchainData { blocks }
     }
 }
 
-impl<'de> Deserialize<'de> for Blockchain {
-    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
-    where
-        D: serde::Deserializer<'de>,
-    {
-        return deserializer.deserialize_map(BlockchainVisitor);
-
-        struct BlockchainVisitor;
-
-        impl<'de> de::Visitor<'de> for BlockchainVisitor {
-            type Value = Blockchain;
-
-            fn expecting(&self, formatter: &mut std::fmt::Formatter) -> std::fmt::Result {
-                formatter.write_str("blockchain struct")
-            }
-
-            fn visit_map<A>(self, mut map: A) -> Result<Self::Value, A::Error>
-            where
-                A: serde::de::MapAccess<'de>,
-            {
-                let blocks_data: Vec<BlockData> = match map.next_entry()? {
-                    Some(("blocks", value)) => value,
-                    _ => return Err(de::Error::missing_field("blocks")),
-                };
-
-                Ok(Blockchain {
-                    blocks: blocks_data
-                        .iter()
-                        .map(|block_data| Block {
-                            payload: block_data.payload.to_owned(),
-                        })
-                        .collect(),
+impl From<BlockchainData> for Blockchain {
+    fn from(blockchain: BlockchainData) -> Self {
+        Self {
+            blocks: blockchain
+                .blocks
+                .into_iter()
+                .map(|block| Block {
+                    payload: block.payload,
                 })
-            }
+                .collect(),
         }
     }
 }
 
-/// Block struct in its serialized form.
-#[derive(Debug, Serialize, Deserialize)]
-struct BlockData<'a> {
-    prev: Option<Hash>,
+#[derive(Debug, Serialize)]
+struct HashInput<'a> {
+    prev: Option<[u8; 32]>,
     payload: &'a str,
-    digest: Hash,
 }
 
-#[derive(Debug)]
-pub struct Block {
-    payload: String,
+fn get_hash(input: &HashInput) -> [u8; 32] {
+    blake3::hash(&bincode::serialize(input).unwrap()).into()
 }
 
-impl Block {
-    pub fn new(payload: &str) -> Self {
-        Self {
-            payload: payload.to_owned(),
+mod digest {
+
+    use serde::{de::Error, Deserializer, Serializer};
+
+    pub fn serialize<S: Serializer>(v: &[u8; 32], s: S) -> Result<S::Ok, S::Error> {
+        opt::serialize(&Some(*v), s)
+    }
+
+    pub fn deserialize<'de, D: Deserializer<'de>>(d: D) -> Result<[u8; 32], D::Error> {
+        Ok(opt::deserialize(d)?.ok_or(Error::custom("missing digest"))?)
+    }
+
+    pub mod opt {
+        use std::convert::TryInto;
+
+        use serde::{de::Error, Deserialize, Deserializer, Serialize, Serializer};
+
+        pub fn serialize<S: Serializer>(v: &Option<[u8; 32]>, s: S) -> Result<S::Ok, S::Error> {
+            let base64 = v.map(base64::encode);
+            base64.serialize(s)
+        }
+
+        pub fn deserialize<'de, D: Deserializer<'de>>(d: D) -> Result<Option<[u8; 32]>, D::Error> {
+            let str = match <Option<String>>::deserialize(d)? {
+                Some(v) => v,
+                None => return Ok(None),
+            };
+            let vec = base64::decode(str).map_err(Error::custom)?;
+            let digest = vec
+                .try_into()
+                .map_err(|_| Error::custom("invalid digest length"))?;
+            Ok(Some(digest))
         }
     }
 }
